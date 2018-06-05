99928ca9a2196ec4fe0f3632592ba16e7d24b073
Raghu Betina <raghu@firstdraft.com>
2018-06-05 19:06:55 +0000

    Direct association between users and courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,9 @@
 class Course < ApplicationRecord
   # Direct associations
 
+  belongs_to :user,
+             :counter_cache => true
+
   # Indirect associations
 
   # Validations
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :courses,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
